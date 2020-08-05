06882657ab92ab10876ea893aac7ec283341cc5a
Adam Gashlin <agashlin@mozilla.com>
2020-08-05 16:50:04 -0700

    Add some stuff

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,28 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 <ul>
 <li><a href="/test.html">/html</a></li>
 <li><a href="/att/test.html">/att/html</a></li>
+<li><a href="/test.shtml">/shtml</a></li>
+<li><a href="/att/test.shtml">/att/shtml</a></li>
+<li><a href="/test.blah">/blah (text/html)</a></li>
+<li><a href="/att/test.blah">/att/blah (text/html)</a></li>
+<li><a href="/test.blah2">/blah2 (application/pdf)</a></li>
+<li><a href="/att/test.blah2">/att/blah2 (application/pdf)</a></li>
+<li><a href="/test.blah3">/blah3 (application/octet-stream)</a></li>
+<li><a href="/att/test.blah3">/att/blah3 (application/octet-stream)</a></li>
 <li><a href="/test.xml">/xml</a></li>
 <li><a href="/att/test.xml">/att/xml</a></li>
-<li><a href="/test.png">/img</a></li>
-<li><a href="/att/test.png">/att/img</a></li>
-<li><a href="/nomime/test.png">/nomime/img</a></li>
+<li><a href="/test-app.xml">/app-xml</a></li>
+<li><a href="/att/test-app.xml">/att/app-xml</a></li>
+<li><a href="/test.png">/png</a></li>
+<li><a href="/att/test.png">/att/png</a></li>
+<li><a href="/nomime/test.png">/nomime/png</a></li>
 <li><a href="/att/nomime/test.png">/att/nomime/img</a></li>
+<li><a href="/test.svg">/svg</a></li>
+<li><a href="/att/test.svg">/att/svg</a></li>
 <li><a href="/test.pdf">/pdf</a></li>
 <li><a href="/att/test.pdf">/att/pdf</a></li>
+<li><a href="/nomime/test.pdf">/nomime/pdf</a></li>
+<li><a href="/att/nomime/test.pdf">/att/nomime/pdf</a></li>
 </ul>"#)).unwrap())
     }
 
@@ -35,6 +49,30 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
                   .body(Body::from("Hello")).unwrap());
     }
 
+    if req.uri().path().ends_with("/test.shtml") {
+        return Ok(builder
+                  .header("Content-Type", "text/html")
+                  .body(Body::from("Hello")).unwrap());
+    }
+
+    if req.uri().path().ends_with("/test.blah") {
+        return Ok(builder
+                  .header("Content-Type", "text/html")
+                  .body(Body::from("Hello")).unwrap());
+    }
+
+    if req.uri().path().ends_with("/test.blah2") {
+        return Ok(builder
+                  .header("Content-Type", "application/pdf")
+                  .body(Body::from("Hello")).unwrap());
+    }
+
+    if req.uri().path().ends_with("/test.blah3") {
+        return Ok(builder
+                  .header("Content-Type", "application/octet-stream")
+                  .body(Body::from("Hello")).unwrap());
+    }
+
     if req.uri().path().ends_with("/test.xml") {
         return Ok(builder
                   .header("Content-Type", "text/xml")
@@ -44,7 +82,23 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 </something>
 "#)).unwrap());
     }
+
+    if req.uri().path().ends_with("/test-app.xml") {
+        return Ok(builder
+                  .header("Content-Type", "application/xml")
+                  .body(Body::from(r#"<?xml version = "1.0" encoding = "utf-8"?>
+
+<something>
+</something>
+"#)).unwrap());
+    }
+
     if req.uri().path().ends_with("/nomime/test.png") {
+       return Ok(builder
+                 .body(Body::from("Hello World!")).unwrap())
+    }
+
+    if req.uri().path().ends_with("/nomime/test.pdf") {
        return Ok(builder
                  .body(Body::from("Hello World!")).unwrap())
     }
@@ -52,6 +106,12 @@ async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     if req.uri().path().ends_with("/test.png") {
        return Ok(builder
                  .header("Content-Type", "image/png")
+                 .body(Body::from("Hello World!")).unwrap())
+    }
+
+    if req.uri().path().ends_with("/test.svg") {
+       return Ok(builder
+                 .header("Content-Type", "image/svg+xml")
                  .body(Body::from("Hello World!")).unwrap())
     }
 
